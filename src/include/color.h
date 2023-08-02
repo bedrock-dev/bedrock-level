c175e817b8f2918bb5303c01f3631ad9622496a5
AgNO3 <33011851+hhhxiao@users.noreply.github.com>
2023-08-02 22:13:41 +0800

    Rename biome color file

--- src/include/color.h
+++ src/include/color.h
@@ -29,7 +29,7 @@ namespace bl {
     [[maybe_unused]] bl::color get_water_color(bl::color gray, bl::biome b);
     //    bl::color get_leave_color(bl::color gray, bl::biome b);
     //    bl::color get_grass_color(bl::color gray, bl::biome b);
-
+    
     [[maybe_unused]] std::unordered_map<std::string, bl::color>& get_block_color_table();
     bool init_biome_color_palette_from_file(const std::string& filename);
 
